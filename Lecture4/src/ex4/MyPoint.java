4d9cab70cae3973b54e354e3b0d9e7a085d90dbd
falconMac <falconhawk@protonmail.com>
2019-05-14 13:19:43 +0900

    Homework Lec 4

---
+++ Lecture4/src/ex4/MyPoint.java
@@ -0,0 +1,45 @@
+package ex4;
+
+public class MyPoint {
+    private int x;
+    private int y;
+
+    MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public double getR() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    public double getTheta() {
+        double radian =  Math.atan2(this.y, this.x);
+        return Math.toDegrees(radian);
+    }
+
+    public double distance(MyPoint anotherPoint) {
+        double deltaX = Math.abs(this.x - anotherPoint.x);
+        double deltaY = Math.abs(this.y - anotherPoint.y);
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+
+    public String toString() {
+        return "a MyPoint(" + getX() + ", " + getY() + ")";
+    }
+
+    public boolean equals(MyPoint aPoint) {
+        boolean ans;
+        ans = (getX() == aPoint.getX()) && (getY() == aPoint.getY());
+        return ans;
+    }
+
+}
