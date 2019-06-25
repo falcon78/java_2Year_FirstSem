0d29ac3d616280da19f53cd9d01d548a8e2da964
Home/Desktop <falconhawk@protonmail.com>
2019-06-25 13:33:02 +0900

    added new lec and refactored a bit

--- Lecture 1/Submit/ArrayTest.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class ArrayTest {
-    public static void main(String[] args){
-        int[] array =  {11,12,13,14,15,16,17,18,19,20};
-        Arrays.stream(array).forEach(System.out::println);
-    }
-}
